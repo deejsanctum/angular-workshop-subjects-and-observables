e85f519af60f85d1a6c50516e15f5190f0057c15
deejsanctum <santos.diogo.j@gmail.com>
2020-12-16 10:23:42 +0000

    step-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,11 @@ import { CurrentTimeStateService } from "./current-time-state.service";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  currentTime$: Observable<Date> = this.currentTimeStateService.currentDate$;
+  currentTime$: Observable<
+    string
+  > = this.currentTimeStateService.currentDate$.pipe(
+    map(newDate => this.getFormattedDate(newDate))
+  );
 
   constructor(private currentTimeStateService: CurrentTimeStateService) {}
 
@@ -17,5 +21,18 @@ export class AppComponent implements OnInit {
     interval(1000).subscribe(time =>
       this.currentTimeStateService.updateCurrentDate(new Date())
     );
+  }
+
+  private getFormattedDate(newDate: Date): string {
+    const hours = `${newDate.getHours()}`.padStart(2, "0");
+    const minutes = `${newDate.getMinutes()}`.padStart(2, "0");
+    const seconds = `${newDate.getSeconds()}`.padStart(2, "0");
+    const formattedDate = `${newDate.getDate()}-${newDate.getMonth() +
+      1}-${newDate.getFullYear()}`;
+    const formattedTime = `${hours}:${minutes}:${seconds}`;
+    return `${formattedDate.padStart(2, "0")} ${formattedTime.padStart(
+      2,
+      "0"
+    )}`;
   }
 }
